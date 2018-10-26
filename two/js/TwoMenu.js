6929e77bb71aa41a0d0009714edd0f7df601f5d8
xiaoyingling <2804024018@qq.com>
2018-10-26 16:10:15 +0800

    二级页面静态

--- two/js/TwoMenu.js
+++ two/js/TwoMenu.js
@@ -1,28 +1,194 @@
-layui.use('laydate', function () {
+/**
+ * 日期选择器
+ */
+$(function(){
 
-    var laydate = layui.laydate;
+    var start = {
+        format: 'YYYY-MM-DD hh:mm:ss',
+        minDate: $.nowDate(0), //设定最小日期为当前日期
+        isinitVal:true,
+        festival:true,
+        ishmsVal:false,
+        maxDate: '2099-06-30 23:59:59', //最大日期
+        choosefun: function(elem,datas){
+            end.minDate = datas; //开始日选好后，重置结束日的最小日期
+        }
+    };
+    var end = {
+        format: 'YYYY年MM月DD日 hh:mm:ss',
+        minDate: $.nowDate(0), //设定最小日期为当前日期
+        festival:true,
+        maxDate: '2099-06-16 23:59:59', //最大日期
+        choosefun: function(elem,datas){
+            start.maxDate = datas; //将结束日的初始值设定为开始日的最大日期
+        }
+    };
 
-    laydate.render({
-        elem: '#time',
-        type: 'datetime',
-        range: '至',
-        format: 'MM/dd HH:mm' 
+    $('#time1').jeDate(start);
+    $('#time2').jeDate(end);
+
+})
+
+
+
+/**
+ * 直播详情里的弹框
+ * 
+ * ModifyName ======= 修改会议名称
+ * 
+ * ModifyScene ====== 修改会议场景
+ * 
+ * AddGuests ======== 添加嘉宾 
+ * 
+ * BriefIntroduction == 编辑简介
+ * 
+ */
+function ModifyName(){
+    $('.modify_name').css("display","block");
+
+    layer.open({
+      type: 1,
+      area: ['480px', '360px'],
+      title: ['', 'background: #fff;border:0'],
+      btn: ['确定','取消'],
+      skin: 'my-skin',
+      btnAlign: 'c',
+      content: $('.modify_name'),
+      cancel: function(index, layero){ 
+        $('.modify_name').css("display","none");
+      },
+      yes: function(index, layero){
+          // 确定
+        var name = $('#Name').val();
+        // 判断名称的长度
+        if(name.length >25){
+          $('.tips').css('display','block');
+        }else{
+          $('#TalkName').text(name);
+          layer.close(index);
+          $('.modify_name').css("display","none");
+        }
+      },
+      btn2: function(index, layero){
+          // 取消
+        layer.close(index);
+        $('.modify_name').css("display","none");
+      },
     });
-});
-
-function oCopy(){
-
-    var link  = $("#link").html();
-    //console.log(aa);
-    link.select(); 
-    document.execCommand("Copy"); // 执行浏览器复制命令
-    alert("已复制好，可贴粘。");
+};
 
 
+function ModifyScene(){
 
-    // obj.select();
-    // js=obj.createTextRange();
-    // js.execCommand("Copy")
-    // alert("复制成功!");
+    layer.open({
+        type: 1,
+        area: ['480px', '360px'],
+        title: ['', 'background: #fff;border:0'],
+        btn: ['确定','取消'],
+        skin: 'my-skin',
+        btnAlign: 'c',
+        content: $('.modify_scene'),
+        cancel: function(index, layero){ 
+            $('.modify_scene').css("display","none");
+          },
+          yes: function(index, layero){
+              layer.close(index);
+              $('.modify_scene').css("display","none");
+          },
+          btn2: function(index, layero){
+              // 取消
+            layer.close(index);
+            $('.modify_scene').css("display","none");
+          },
+    });
+
+};
+
+function AddGuests(){
+    $('.add_guest').css("display","block");
+
+    layer.open({
+      type: 1,
+      area: ['480px', '360px'],
+      title: ['', 'background: #fff;border:0'],
+      btn: ['确定','取消'],
+      skin: 'my-skin',
+      btnAlign: 'c',
+      content: $('#add_guest'),
+      cancel: function(index, layero){ 
+        $('#add_guest').css("display","none");
+      },
+      yes: function(index, layero){
+        var add_name = $('#Adds').val();
+        // 判断嘉宾姓名的长度
+        if(add_name.length >20){
+          $('.tips_2').css('display','block');
+        }else{
+          layer.close(index);
+          $('#add_guest').css("display","none");
+        }
+      },
+      btn2: function(index, layero){
+          // 取消
+        layer.close(index);
+        $('#add_guest').css("display","none");
+      },
+    });
+};
+
+function BriefIntroduction(){
+
+    var old = $('#text_brief').text();
+    console.log(old);
+    $('#brief').val(old);
+
+    $('.brief_introduction').css("display","block");
+
+    layer.open({
+      type: 1,
+      area: ['700px', '430px'],
+      title: ['', 'background: #fff;border:0'],
+      btn: ['确定','取消'],
+      skin: 'my-skin',
+      btnAlign: 'c',
+      content: $('.brief_introduction'),
+      cancel: function(index, layero){ 
+        $('.brief_introduction').css("display","none");
+      },
+      yes: function(index, layero){
+
+          var news = $('#brief').val();
+        
+        // 判断简介的长度
+        if(news.length >50){
+          $('.tips_3').css('display','block');
+        }else{
+          $('#text_brief').text(news);
+          layer.close(index);
+          $('.brief_introduction').css("display","none");
+        }
+      },
+      btn2: function(index, layero){
+          // 取消
+        layer.close(index);
+        $('.brief_introduction').css("display","none");
+      },
+    });
+
 }
-    
+
+
+
+/**
+ * 直播详情页面
+ * 
+ * 复制链接
+ */
+// function copyUrl2(){
+//     var Url2=document.getElementById("links");
+//     Url2.document.execCommand("Copy");// 选择对象
+//      // 执行浏览器复制命令
+//     alert("已复制好，可贴粘。");
+//  }
+
+
